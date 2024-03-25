039204cd523693703f8047d682f56a5a790531c9
Francis Vianney Salvamante <salvamante2012@gmail.com>
2024-03-25 20:08:49 +0800

    updated routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,7 +4,7 @@ import todosRoute from "./todos.route";
 const router = Router();
 
 router.use("/todos", todosRoute);
-router.use("/", (req: Request, res: Response, next: NextFunction) => {
+router.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send({ message: "Test route successful" });
 });
 
