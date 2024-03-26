bb3524831b250634ce830bb62299bb43b2715b57
Francis Vianney Salvamante <salvamante2012@gmail.com>
2024-03-26 23:14:42 +0800

    Updated index.ts

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -30,3 +30,4 @@ app.use("/api", routes_1.default);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+exports.default = app;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -33,3 +33,5 @@ app.use("/api", routes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+export default app;
